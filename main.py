25b5b3cfda260d482f9cf5707b36b03b17f49629
SimenBU <123465276+SimenBU@users.noreply.github.com>
2023-02-10 10:59:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def on_forever():
     Lydnivå_Verdi
     Lydnivå_Verdi = ReadSensor()
     serial.write_value(Lyd, Lydnivå_Verdi)
-    basic.pause(6000)
+    basic.pause(1000)
 basic.forever(on_forever)
